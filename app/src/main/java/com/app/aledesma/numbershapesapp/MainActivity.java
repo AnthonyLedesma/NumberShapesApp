75033518567851320c5ac9c4faca09f9217bc2f8
paranoia1906 <anthonymledesma@gmail.com>
2018-09-27 08:44:02 -0700

    Finished logic for NumberShapesApp. Answer is given by Toast.

--- app/src/main/java/com/app/aledesma/numbershapesapp/MainActivity.java
+++ app/src/main/java/com/app/aledesma/numbershapesapp/MainActivity.java
@@ -2,8 +2,51 @@ package com.app.aledesma.numbershapesapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+        class GivenNumber {
+            int x;
+
+            boolean isPerfectSquare(double x) {
+
+                // Find floating point value of
+                // square root of x.
+                double sr = Math.sqrt(x);
+
+                // If square root is an integer
+                return ((sr - Math.floor(sr)) == 0);
+            }
+
+            boolean isTriangularNumber(long num) {
+                long calc_num = 8 * num + 1;
+                long t = (long) Math.sqrt(calc_num);
+                if (t * t == calc_num) {
+                    return true;
+                }
+                return false;
+            }
+        }
+
+
+            public void checkNum(View view){
+                EditText userInput = (EditText) findViewById(R.id.UserInput);
+
+                GivenNumber anthony = new GivenNumber();
+                anthony.x = Integer.parseInt(userInput.getText().toString());
+
+                String output = String.format(
+                        "You entered the number %d. It is %s that the number is a perfect square and it is %s that the number is a triangular number",
+                        anthony.x, anthony.isPerfectSquare(anthony.x), anthony.isTriangularNumber(anthony.x));
+                Toast.makeText(this, output, Toast.LENGTH_SHORT).show();
+            }
+
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
